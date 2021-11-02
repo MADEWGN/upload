7705e58173bc5fe1a10721fb7c908f50043d5c55
Made Wiguna <90078192+MADEWGN@users.noreply.github.com>
2021-11-02 09:00:52 +0800

    Create data.php

---
+++ data.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Simpan Data</title>
+</head>
+<body>
+<h2> HUGO CMS - PHP </h2>
+<table>
+<form method="post" action="">
+    <tr><td>Nama</td><td><input type="mardown" name="nama" style="width:300px"></td></tr>
+<tr><td>gambar</td><td><input type="text" name="gambar" style="width:300px"></td></tr>
+    <tr><td>Isi</td><td><textarea name="data" style="width:300px"></textarea></td></tr>
+    <tr><td></td><td><input type="submit" name="ok" value="Simpan"></td></tr>
+</form>
+</table>
+
+ <a href="/post/hasil.html"nofollow noopener noreferrer">berhasil di simpan</a>
+
+</body>
+</html>
+
+<?php
+if(isset($_POST['ok'])){
+    if(empty($_POST['nama']) || empty($_POST['data'])){
+        print "Lengkapi form";
+    }else{
+        $nama=$_POST['nama'];
+        $data=$_POST['data'];
+        $tanggal=date("h:i:s");
+        $buka=fopen("post/hasil.html",'a');
+        fwrite($buka,"title: ${nama} <br> ");
+        fwrite($buka,"dibuat : ${tanggal} <br> ");
+        fwrite($buka," isi : ${data} <br> ");
+        fwrite($buka,"<hr>");
+        fclose($buka);
+        print "data berhasil disimpan";
+    }
+}?>
